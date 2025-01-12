c42d1ff906f0e888c74a4a17528b91d5d4463d95
Thilina Sandaruwan <tsandaruwan141@gmail.com>
2025-01-12 23:14:16 +0530

    advertising php

---
+++ advertising.php
@@ -0,0 +1,37 @@
+<?php
+
+$servername = "localhost";
+
+$username = "root";
+
+$password = "";
+
+$dbname = "advertising";
+
+
+ 
+
+// Create connection
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+
+
+if ($conn->connect_error) {
+
+die("Connection failed: " . $conn->connect_error);
+
+}else{
+
+echo "Connected successfully";
+
+}
+?>
+
+
+
+
+
+
+
